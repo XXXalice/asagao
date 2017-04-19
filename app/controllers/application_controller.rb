f747cac78bec7f237cd70173bc8f1ba2030ddc9a
umemiya <umemiya@amamiyayuuichirou-no-MacBook-Pro.local>
2017-04-19 17:58:20 +0900

    add about views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+  def current_member
+    Member.find_by(id: session[:member_id]) if session[:member_id]
+  end
+  helper_method :current_member
 end
