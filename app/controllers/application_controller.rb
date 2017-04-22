e5a8b5ea775018097aa00ae04c56d795738903f0
umemiya <umemiya@amamiyayuuichirou-no-MacBook-Pro.local>
2017-04-23 00:49:42 +0900

    add occer Forbidden if not login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  class Forbidden < StandardError; end
+
   private
   def current_member
     Member.find_by(id: session[:member_id]) if session[:member_id]
   end
   helper_method :current_member
+
+  def login_reqauired
+    raise Forbidden unless current_member
+  end
 end
--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,4 +1,5 @@
 class MembersController < ApplicationController
+  before_action :login_required
 
   def index
     @members = Member.order("number")
