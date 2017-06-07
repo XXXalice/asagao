0d6bfd4b9f59402b128861aeab7a73d3273b3b63
umemiya <umemiya@dot1x0283.14-192-19.wlan.ms.dendai.ac.jp>
2017-06-07 14:29:30 +0900

    add errorpage 400 403 404 500

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   class Forbidden < StandardError; end
+  class NotFound < StandardError; end
+
+  if Rails.env.production?
+    rescue_from Exception, with: :rescue_500
+    rescue_from ActionController::RoutingError, with: :rescue_404
+    rescue_from ActiveRecord::RecordNotFound, with: :rescue_404
+    rescue_from ActionController::ParameterMissing, with: :rescue_400
+  end
+
+  rescue_from Forbidden, with: :rescue_403
+  rescue_from NotFound, with: :rescue_404
 
   private
   def current_member
@@ -12,4 +23,21 @@ class ApplicationController < ActionController::Base
   def login_required
     raise Forbidden unless current_member
   end
+
+  def rescue_400(exception)
+    render "errors/bad_request", status: 400, layout: "error", formats: [:html]
+  end
+
+  def rescue_403(exception)
+    render "errors/forbidden", status: 403, layout: "error", formats: [:html]
+  end
+
+  def rescue_404(exception)
+    render "errors/not_found", status: 404, layout: "error", formats: [:html]
+  end
+
+  def rescue_500(exception)
+    render "errors/internal_server_error", status: 500, layout: "error", formats: [:html]
+  end
+
 end
