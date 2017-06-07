1877aab9a1bbbe34f2dd1bd6d609bb6f0bbdf773
umemiya <umemiya@dot1x0283.14-192-19.wlan.ms.dendai.ac.jp>
2017-06-07 14:44:45 +0900

    add define 400 500

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "top#index"
   get "about" => "top#about" , as: "about"
+  get "bad_request" => "top#bad_request"
+  get "internal_server_error" => "top#internal_server_error"
   get "lesson/:action(/:name)" => "lesson"
 
   resources :members do
